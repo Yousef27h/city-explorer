704bf1d8c11267bb713d4e4ecd7d857db68238b1
Yousef27h <yousef_salem97@yahoo.com>
2021-05-04 18:41:52 +0300

    lab06 & lab07

---
+++ README.md
@@ -0,0 +1,75 @@
+# City Explorer
+
+**Author**: Yousef Salem
+
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+
+In my project, I built a web page that displays location information and renders a site map depending on the user's input, using Location IQ Geocoding API. The reason behind this project is to provide a tool that helps people by providing location information based on user's input the of location name, also rendering a map of the site.
+
+## Getting Started
+
+1. First we have to create our repository, and add react app inside of it.
+2. Build static page of the page components without any interactivity between them.
+3. Implement API, to get our data base from.
+4. Make the page interactive by adding state to it, and use the state to render the value of the form in our page.
+
+## Architecture
+
+In this project HTML, CSS, and JavaScript was used to build it. Multiple librarys were used  such as React JS, Axios that helps us get data from external resources.
+The basic design for this web page is illustrated in the wireframe below
+
+![wireframe](./src/assets/wireframe.JPG)
+
+
+## Change Log
+
+03-05-2021 10:00pm: My project has a react application containing a form, and connected with a web server API to get data from depending on user input to form.
+04-05-2021 07:00pm: Application now has a fully-functional express server, with a GET route for the location resource.
+
+## Credit and Collaborations
+
+- [React-bootstrap](https://react-bootstrap.netlify.app/) was used to style my web page.
+- [Location IQ](https://locationiq.com/) as a web server API to retrieve data from.
+- [npm](https://www.npmjs.com/) to download packages I used for my project.
+- [Wire Frame](https://wireframe.cc/) was used to create wire frame for my web page.
+- [Fonts Awesome](https://fontawesome.com/) where I got my icons from.
+- [Color Palette](https://coolors.co/palettes/trending), where you can find my color set.
+- [Dania Abughoush](https://github.com/DaniaAbughoush) has reviewed my code in lab-06.
+- [Sufian Hamdan](https://github.com/SufianHamdan) has reviewed my code in lab-07.
+
+
+## Time Estimation
+
+### Lab 06
+
+Name of feature: Asynchronous code, and APIs
+
+Estimate of time needed to complete: 5 hours
+
+Start time: 08:00pm 
+
+Finish time: 01:30am
+Actual time needed to complete: 5.5 hours
+
+_web request-response cycle for the current lab_
+![WRRC](./src/assets/lab06.png)
+
+
+### Lab 07
+
+Name of feature: Custom Servers with Node and Express
+
+Estimate of time needed to complete: 3 hours
+
+Start time: 10:00pm 
+
+Finish time: 12:30:00am
+Actual time needed to complete: 2.5 hours
+
+_web request-response cycle for the current lab_
+
+![WRRC](./src/assets/lab07.JPG)
+
+
--- src/App.js
+++ src/App.js
@@ -1,69 +1,96 @@
 import React from "react";
 import "./App.css";
-import { Form, Button } from "react-bootstrap";
 import axios from "axios";
-import Card from 'react-bootstrap/Card';
-import earth from './assets/earth.png';
+import earth from "./assets/earth.png";
+import CardClass from "./components/card";
+import SearchForm from "./components/form";
+import Weather from "./components/weather";
+import Error from "./components/error";
 
 class App extends React.Component {
-constructor(props){
-  super(props);
-  this.state={
-    searchInput: "",
-    data : "",
-    show: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: "",
+      data: "",
+      weatherData: "",
+      show: false,
+      dataStatus: 200,
+    };
   }
-}
 
-handleSubmit = async (event)=>{
-  event.preventDefault();
-  const url = `https://us1.locationiq.com/v1/search.php?key=pk.8df7f8bc5dee816b49ba58eb0d0e7367&q=${this.state.searchInput}&format=json`;
-  const req = await axios.get(url);
-  this.setState({
-    data: req.data[0],
-    show: true
-  })
- console.log(req.data[0])
- if (this.state.data !== null){
-   this.setState({
-     show : true,
-   })
- }
-}
-handleText = (event)=>{
-this.setState({
-  searchInput : event.target.value
-})
-}
-render(){
-  return (
-    <div>
-    <header>
-    
-      <img src={earth} style={{ width: '6rem'}} className="logo"/>
-      <h1 className="title"> City Explorer</h1>
-      </header>
-      <main></main>
-      <Form onSubmit={this.handleSubmit} className="form">
-        <Form.Label for="textForm">Location Name</Form.Label> <br />
-        <Form.Control type="text" onChange={this.handleText} required id="textForm"/>
-        <Button type="submit" id="button">Explore!</Button>
-      </Form>
-      {this.state.show ? 
-      (
-      <Card style={{ width: '18rem'}} className="card">
-      <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.8df7f8bc5dee816b49ba58eb0d0e7367&center=${this.state.data.lat},${this.state.data.lon}`} width="300px" id="cardImg"/>
-      <Card.Body>
-      <Card.Title>{this.state.data.display_name}</Card.Title>
-      <Card.Text>
-      Longitude: {this.state.data.lon}<br />
-      Latitude: {this.state.data.lat}
-      </Card.Text>
-       </Card.Body>
-      </Card>) : null}
-    </div> 
-  );
-}
+  handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const url = `https://us1.locationiq.com/v1/search.php?key=pk.8df7f8bc5dee816b49ba58eb0d0e7367&q=${this.state.searchInput}&format=json`;
+      const myApi = await axios.get("http://localhost:3001/weather");
+      console.log(myApi);
+      console.log(myApi.data);
+      const req = await axios.get(url);
+      this.setState({
+        data: req.data[0],
+        show: true,
+        weatherData: myApi.data,
+      });
+      console.log(req.data[0]);
+      if (this.state.data !== null) {
+        this.setState({
+          show: true,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        show: false,
+        dataErorr: error.response.data.error,
+        dataStatus: error.response.status,
+      });
+      console.log(this.state);
+    }
+  };
+
+  handleText = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <header>
+          <img src={earth} style={{ width: "6rem" }} className="logo" />
+          <h1 className="title"> City Explorer</h1>
+          <SearchForm
+            handleSubmit={this.handleSubmit}
+            handleText={this.handleText}
+          />
+        </header>
+        <main>
+          {this.state.dataStatus !== 200 && (
+            <Error
+              dataStatus={this.state.dataStatus}
+              dataErorr={this.state.dataErorr}
+            />
+          )}
+          {this.state.show && (
+            <>
+              <CardClass
+                lat={this.state.data.lat}
+                lon={this.state.data.lon}
+                display_name={this.state.data.display_name}
+              />
+              <Weather weatherData={this.state.weatherData} />
+            </>
+          )}
+        </main>
+        <footer>
+          <p>
+            &copy; <b>Yousef Salem</b>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
